dd329de50e04bdd681f83f3cefead1745c6a3906
Hazmi Abdun Nazir <hello@hazmi.id>
2015-02-27 09:55:06 +0700

    initial commit, put everything in github

---
+++ assets/js/app-footer.jsx
@@ -0,0 +1,14 @@
+var App = App || {};
+
+App.ColorFooter = (function(){
+    var self = React.createClass({
+        render: function(){
+            return (
+                <div id="color-footer">
+                    footer
+                </div>
+            )
+        }
+    });
+    return self;
+})();
